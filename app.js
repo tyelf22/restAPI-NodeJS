9f10718447d2e275520927c401d78d28b79ab8ec
tyson <tyelf22@hotmail.com>
2020-02-23 22:06:14 -0700

    edits for heroku

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.put("/player/:id", PlayerController.edit)
 app.delete("/player/:id", PlayerController.delete)
 
 //Listen on port 3000
-app.listen(3000, () => {
-    console.log("Listening at :3000....")
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Listening at ${PORT}....`)
 })
